ec830b39a5c012c21ff07720f8170b1ee55f25fc
Romain <r.huc@orange.fr>
2023-01-16 11:37:25 +0100

    add back feature

--- app/src/main/java/org/o7planning/flagquizz/mainQuestionnaireActivity.java
+++ app/src/main/java/org/o7planning/flagquizz/mainQuestionnaireActivity.java
@@ -4,12 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class mainQuestionnaireActivity extends AppCompatActivity {
     final String VILLE = "ville";
-
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle("Quittez l'application ?")
+                .setMessage("Etes vous sur de vouloir quitter l'application ?")
+                .setNegativeButton(android.R.string.no, null)
+                .setPositiveButton(android.R.string.yes, (arg0, arg1) -> {
+                    finish();
+                    System.exit(0);
+                }).create().show();
+    }
 
     @Override
 
